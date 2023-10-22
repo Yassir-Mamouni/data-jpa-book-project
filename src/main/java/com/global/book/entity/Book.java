4a1922258c04f58d79b86767a0bd31cbe1e960e8
Yassir Mamouni <mamouniyassir1@gmail.com>
2023-10-22 18:31:33 +0100

    create project

---
+++ src/main/java/com/global/book/entity/Book.java
@@ -0,0 +1,48 @@
+package com.global.book.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
