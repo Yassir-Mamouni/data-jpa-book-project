041e79009d7d4f0b32df8428f05da7283087d569
Yassir Mamouni <mamouniyassir1@gmail.com>
2023-10-23 19:01:27 +0100

    add book service and controller

---
+++ src/main/java/com/global/book/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.global.book.controller;
+
+
+import com.global.book.entity.Book;
+import com.global.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    private BookService bookService;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(bookService.findById(id));
+    }
+    @GetMapping()
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok(bookService.findAll());
+    }
+    @PostMapping()
+    public ResponseEntity<?> insert(@RequestBody Book entity){
+        return ResponseEntity.ok(bookService.insert(entity));
+    }
+    @PutMapping()
+        public ResponseEntity<?> update(Book entity){
+        return ResponseEntity.ok(bookService.update(entity));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        bookService.deleteById(id);
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/global/book/service/BookService.java
@@ -0,0 +1,38 @@
+package com.global.book.service;
+
+
+import com.global.book.entity.Book;
+import com.global.book.repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private BookRepo bookRepo;
+    @Autowired
+    public BookService(BookRepo bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+    public Book findById(Long id){
+        return bookRepo.findById(id).orElseThrow();
+    }
+    public List<Book> findAll(){
+        return bookRepo.findAll();
+    }
+    public Book insert(Book entity){
+        if(entity.getId() != null){
+            throw new RuntimeException();
+        }
+        return bookRepo.save(entity);
+    }
+    public Book update(Book entity){
+        Book book = findById(entity.getId());
+        book.setName(entity.getName());
+        return bookRepo.save(book);
+    }
+    public void deleteById(Long id){
+        bookRepo.deleteById(id);
+    }
+}
