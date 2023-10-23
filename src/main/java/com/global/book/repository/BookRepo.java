5812c99d67a71e7cb2289ac3a9edc5aa449e8aeb
Yassir Mamouni <mamouniyassir1@gmail.com>
2023-10-23 22:29:16 +0100

    create load graph for author

--- src/main/java/com/global/book/repository/BookRepo.java
+++ src/main/java/com/global/book/repository/BookRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,8 @@ public interface BookRepo extends JpaRepository<Book,Long> {
     @Override
     @EntityGraph(attributePaths = {"author"})
     Optional<Book> findById(Long aLong);
+
+    @Override
+    @EntityGraph(attributePaths = {"author"})
+    List<Book> findAll();
 }
